4c84752cc1c6d4b4bc6e11e69e7053fabe97ccc6
Ferran Blanché <ferran.blanche@gmail.com>
2023-02-03 22:56:12 +0100

    feat: :sparkles: Add todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,26 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import "@picocss/pico";
+  import "./styles/theme.css";
+
+  let todos: string[] = ["todo #1", "todo #2", "todo #3"];
+
+  function handleSubmit(event) {
+    let todo = event.target.todo.value;
+    todos = [...todos, todo]
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="container">
+  <article>
+    <h1>Todo list</h1>
+    <form on:submit|preventDefault={handleSubmit}>
+      <input id="todo" type="text" placeholder="What needs to be done?" autofocus />
+      <button>Add</button>
+    </form>
+    <ul>
+      {#each todos as todo}
+        <li>{todo}</li>
+      {/each}
+    </ul>
+  </article>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
